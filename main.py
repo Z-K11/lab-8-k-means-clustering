38b2137a4afd76470c5a07c1844ff71d1c6cbfd1
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-01-15 21:37:09 +0500

    create synthetic data using make_blobs function

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ from sklearn.datasets import make_blobs
 '''The make_blobs function from scikit-learn it is used to generate synthetic data for clustering mostly used for testing and demonstrating clustering 
 algorithms like Kmeans which we will be using in this lab'''
 np.random.seed(11)
+x,y = make_blobs(n_samples=5000,centers=[[4,4],[-2,1],[2,-3],[1,1]],cluster_std=0.9)
+'''n_samples is obviously number of samples which is the same as data points ?
+centers [x,y] format means the space in which the samples are placed is 2d here we will have 4 clusters at the given centers and the points will spread
+across them according to guassian normal distribution with a standard deviation of 0.9'''
