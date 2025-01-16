47068f52defd588bab5067c4518e4b939845f72d
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-01-16 22:56:02 +0500

    visualize the cluster and centroids Now the centroids and clusters are plotted correctly with different colors

--- main.py
+++ main.py
@@ -31,3 +31,21 @@ fig =plt.figure(figsize=(6,4))
 '''matplotlib figure, the size is given in inches 6 length,height 4 inches'''
 colors=plt.cm.Spectral(np.linspace(0,1,(len(set(k_means_labels)))))
 print("Colors :\n",colors)
+ax=fig.add_subplot(1,1,1)
+for k,col in zip(range(len([[4,4],[-2,1],[2,-3],[1,1]])),colors):
+    '''zip creates iteratable pair If colors = ['red', 'blue', 'green', 'yellow'], zip(...) would produce (0, 'red'), (1, 'blue'),etc
+    length calculates the number of rows and range generates numbers from 0 for the rows i.e indices'''
+    my_members = (k_means_labels==k)
+    cluster_center=k_means_cluster_center[k]
+    ax.plot(x[my_members,0],x[my_members,1],'w',markerfacecolor=col,marker='.')
+    '''my_members includes the value of a lebel which can be from 0 - 3 in our case '''
+    ax.plot(cluster_center[0],cluster_center[1],'o',markerfacecolor=col,markeredgecolor='k',markersize=6)
+    '''cluster_center[0] and cluster_center[1]: Coordinates of the cluster centroid (x and y).
+    'o': Displays the centroid as a circle marker.
+    markerfacecolor=col: Sets the centroid's fill color to match the cluster's color.
+    markeredgecolor='k': Adds a black outline around the centroid marker for emphasis.
+    markersize=6: Sets the size of the centroid marker.'''
+ax.set_title('Kmeans')
+ax.set_xticks(())
+ax.set_yticks(())
+plt.savefig('./pngFiles/kmeans_plot.png')
